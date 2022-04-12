34bee6b6d5dfa6c5dd0ae76714af388f81172735
MiguelBarreraDev <barreram987@gmail.com>
2022-04-12 16:49:03 -0500

    Add: Methods to interact with the database

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,20 +1,16 @@
 """Module that define engine of storage in database"""
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from base_model import Base
 
 
 def connect(**kwrgs):
     """Make link to the MySQL"""
-    from models.__init__ import config   
+    from models.__init__ import config
     url = "mysql+mysqldb://{}:{}@{}/{}".format(
         config["user"], config["passwd"], config["host"], config["db"]
     )
     engine = create_engine(url, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return {"engine": engine, "session": session}
+    return engine
 
 
 class DBStorage:
@@ -24,12 +20,28 @@ class DBStorage:
 
     def __init__(self):
         """Class constructor"""
-        from models.__init__ import config   
-        self.__engine = connect()["engine"]
-        self.__session = connect()["session"]
+        from models.__init__ import config
+        self.__engine = connect()
         if config["env"] == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query for All objects depending of the class name"""
-        if not cls:
+        pass
+
+    def new(self, obj):
+        """Add the object to the current database session"""
+        if obj:
+            self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete from the current database session obj if not None"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create all tables in the database and the current session"""
+        pass
