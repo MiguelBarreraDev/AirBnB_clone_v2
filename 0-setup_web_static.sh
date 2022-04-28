8db85be4935cc7ae56a43bf0534b171afda04dcf
MiguelBarreraDev <barreram987@gmail.com>
2022-04-28 18:22:02 -0500

    Update comments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-## Script that sets up your web servers for the deployment of web_static
-### Update of the repositories and run upgrade
+# Script that sets up your web servers for the deployment of web_static
+## Update of the repositories and run upgrade
 sudo apt-get -y update
 sudo apt-get -y upgrade
-### Install Nginx web server
+## Install Nginx web server
 sudo apt-get -y install nginx
-### Create directories if they don't exist
+## Create directories if they don't exist
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-### Adding fake content in the index.html file
+## Adding fake content in the index.html file
 sudo echo "
 html>
   <head>
@@ -18,12 +18,12 @@ html>
   </body>
 </html>
 " > /data/web_static/releases/test/index.html
-### Create symbolic link
-ln -fs /data/web_static/releases/test/ /data/web_static/current/
-### Modify owner and group of the data directory
+## Create symbolic link
+sudo ln -fs /data/web_static/releases/test /data/web_static/current
+## Modify owner and group of the data directory
 sudo chown -hR ubuntu:ubuntu /data/
-### Adding an alias in the configuration of Nginx
-conf="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
-sed -i '56i $conf' /etc/nginx/sites-available/default
-### Restart service of the web sever
+## Adding an alias in the configuration of Nginx
+conf="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "56i $conf" /etc/nginx/sites-available/default
+## Restart service of the web sever
 sudo service nginx restart
