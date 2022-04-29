5060ee224bf39e39fde4142b26d08672680a1690
MiguelBarreraDev <barreram987@gmail.com>
2022-04-28 20:02:15 -0500

    Update return value

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -15,10 +15,10 @@ def do_pack():
     Function definition that generates a .tgz archive
     """
     try:
-        local("mkdir -p ./versions/")
+        local("mkdir -p versions")
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        dest = "./versions/web_static_{}.tgz".format(date)
-        local("tar -cvf {} ./web_static".format(dest))
+        dest = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvf {} web_static".format(dest))
         return dest
     except Exception:
         return None
