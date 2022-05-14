b0811c68a49506f13d5b6e571be9ae2a88616066
MiguelBarreraDev <barreram987@gmail.com>
2022-05-14 14:40:13 -0500

    Update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,15 +2,8 @@
 """Write a script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def appcontext(self):
-    """Remove the current SQLAlchemy Session"""
-    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -18,6 +11,12 @@ def display_page():
     """Display page html"""
     all_states = storage.all('State').values()
     return render_template("7-states_list.html", states=all_states)
+
+
+@app.teardown_appcontext
+def appcontext(self):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == "__main__":
