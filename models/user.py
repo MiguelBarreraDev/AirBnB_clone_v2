35b1b4558f3517bd1c2bd9a355193c53bae200df
MiguelBarreraDev <barreram987@gmail.com>
2022-05-15 03:16:18 -0500

    Refactor code

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Column, String
+from os import getenv
 
 
 class User(BaseModel, Base):
@@ -14,8 +14,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
-        places = relationship('Place', backref='user', cascade='all, delete')
-        reviews = relationship('Review', backref='user', cascade='all, delete')
+        places = relationship('Place', backref='user')
+        reviews = relationship('Review', backref='user')
     else:
         email = ''
         password = ''
