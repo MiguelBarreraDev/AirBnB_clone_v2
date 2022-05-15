57d5d9d6c9a2086bedde14b97172dfff32569b50
MiguelBarreraDev <barreram987@gmail.com>
2022-05-15 03:55:35 -0500

    Update: Refactor code

--- models/user.py
+++ models/user.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Column, String
 from os import getenv
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """
+    Model of the User table in the database
+    """
     __tablename__ = "users"
     if getenv('HBNB_TYPE_STORAGE') == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         places = relationship('Place', backref='user')
         reviews = relationship('Review', backref='user')
     else:
